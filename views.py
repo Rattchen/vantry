ee0643b4e600a035597936281f0eaa0922553ba7
Rättchen <57540790+Rattchen@users.noreply.github.com>
2025-04-12 18:04:02 +0200

    Simple home page

--- views.py
+++ views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.views.generic import DetailView, TemplateView
+from .models import Product
 
-# Create your views here.
+class HomeView(TemplateView):
+    template_name = 'vantry/home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["message"] = "Welcome to Vantry!"
+        return context
+    
