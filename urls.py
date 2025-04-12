f8350ae6b7a6eb0eaa72fb16b8e18f3763343724
Rättchen <57540790+Rattchen@users.noreply.github.com>
2025-04-12 23:42:31 +0200

    Add hardcoded OpenFoodFacts call

--- urls.py
+++ urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
     path('product/<int:pk>', views.ProductDetailView.as_view(), name='product_details'),
+    path('product/off/<int>', views.OFFView.as_view(), name='product_off'),
 ]
--- views.py
+++ views.py
@@ -1,4 +1,5 @@
 from django.views.generic import DetailView, TemplateView
+from openfoodfacts import API, APIVersion, Country, Environment, Flavor
 from .models import Product
 
 class HomeView(TemplateView):
@@ -16,4 +17,22 @@ class ProductDetailView(DetailView):
 
     ## If obj.off_id -> return OFF item
     ## Else -> return DB item + info how to add an item
-        
+
+class OFFView(TemplateView):
+    template_name = 'vantry/off.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        api = API(user_agent="Vantry",
+            username=None,
+            password=None,
+            country=Country.pl,
+            flavor=Flavor.off,
+            version=APIVersion.v2,
+            environment=Environment.net
+            )
+        code = "5900500047459"
+        item = api.product.get(code, fields=['product_name', 'nutriments', 'generic_name', 'brands', 'brands_hierarchy'])
+        context["item"] = item
+        print(item)
+        return context
